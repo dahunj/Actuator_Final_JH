75b3b9128784fa5419e00eae5acc43f4c13a57e9
dahunj <dahunj@nate.com>
2026-08-21 09:12:42 +0900

    추가

--- CMAI2100F2 - v2.7.1/CMAI2100Dlg.cpp
+++ CMAI2100F2 - v2.7.1/CMAI2100Dlg.cpp
@@ -708,13 +708,13 @@ void CCMAI2100Dlg::Set_DownAction()
 		return;
 	}
 	int nState = gData.m_nMS;
-	if (nState == STATE_NONE || nState == STATE_RUN || nState == STATE_LOTEND || nState == STATE_INITEND || (nState == STATE_READY && !gDown.bDownHappen && gDown.bDownClear))  
+	if (nState == STATE_NONE || nState == STATE_RUN || nState == STATE_LOTEND || nState == STATE_INITEND || (nState == STATE_READY && !gDown.bDownHappen && gDown.bDownClear) || (nState == STATE_ALARM && !gDown.bDownHappen && gDown.bDownClear))  
 	{ 
 		m_dwDownActionTime = GetTickCount(); 
 		return; 
 	}
 
-	if(gDown.bDownHappen && !gDown.bDownClear && !g_dlgDownReport.m_bStart)
+	if(gDown.bDownHappen && !gDown.bDownClear && !g_dlgDownReport.m_bStart )
 	{		
 		gDown.bDownHappen = FALSE;
 		m_dwDownActionTime = GetTickCount(); 
